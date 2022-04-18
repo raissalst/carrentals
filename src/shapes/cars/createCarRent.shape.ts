ed3590d55a336a0318f43739f1005dddfbdaff28
gustavolira8813 <gustavolira8813@gmail.com>
2022-04-18 11:23:06 -0300

    feature: shape createCarRental created

---
+++ src/shapes/cars/createCarRent.shape.ts
@@ -0,0 +1,11 @@
+import * as yup from 'yup';
+
+const createCarRentShape = yup.object().shape({
+  rentalStartDate: yup
+    .date()
+    .required()
+    .default(() => new Date()),
+  rentalReturnDate: yup.date().required(),
+});
+
+export default createCarRentShape;
