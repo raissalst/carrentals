5ec8a31faf8e822ca51c5cd071232c66dd139734
Raissa Toledo <raissalst@gmail.com>
2022-04-18 11:39:38 -0300

    feat: test working

--- src/tests/shapes/users/updateUser.spec.ts
+++ src/tests/shapes/users/updateUser.spec.ts
@@ -1,17 +1,17 @@
 import { describe, expect, it } from '@jest/globals';
-import { updateUserShape } from '../../shapes';
-
-const schemaRight = {
-  name: 'novo nome',
-  phone: '55976885423',
-  cpf: '119.987.647-03',
-};
-
-const schemaWrong = {
-  cpf: '11987648309',
-};
+import { updateUserShape } from '../../../shapes';
 
 describe('verify user update shape', () => {
+  const schemaRight = {
+    name: 'novo nome',
+    phone: '55976885423',
+    cpf: '119.987.647-03',
+  };
+
+  const schemaWrong = {
+    cpf: '11987648309',
+  };
+
   it('should be a valid schema', async () => {
     const result = await updateUserShape.isValid(schemaRight);
     expect(result).toEqual(true);
