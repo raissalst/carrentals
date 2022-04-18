3d32ec646f74291fcd1e2223a7b9e0d380d1a6f7
Tulio <tulio.gp2@gmail.com>
2022-04-18 19:17:46 +0200

    refactor: save for pull develop

--- src/tests/shapes/user/createUser.spec.ts
+++ src/tests/shapes/user/createUser.spec.ts
@@ -1,2 +1,6 @@
 import { describe, expect, test } from '@jest/globals';
 import request from 'supertest';
+import app from '../../../app';
+import { USERS } from '../../../configs';
+import { generateUser, baseURL } from '../../dataToUseInTests';
+
