93c71343b4c7be91d7e81eee9df7bc17f0771762
Tulio <tulio.gp2@gmail.com>
2022-04-19 20:59:50 +0200

    fix: reedidet imports on shapes/index

--- src/shapes/index.ts
+++ src/shapes/index.ts
@@ -1,5 +1,7 @@
-import createUserShape from "./users/createUser.shape";
 import updateUserShape from './users/updateUser.shape';
 import loginUserShape from './users/loginUser.shape';
+import createCarShape from './cars/createCar.shape';
+import createUserShape from "./users/createUser.shape";
+import createCarRentShape from './cars/createCarRent.shape';
 
-export { updateUserShape, loginUserShape, createUserShape };
+export { updateUserShape, loginUserShape, createCarRentShape, createCarShape, createUserShape };
