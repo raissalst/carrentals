63b4ba9a57ff176ff7b7c01180f8130d633217f6
Raissa Toledo <raissalst@gmail.com>
2022-04-19 13:04:36 -0300

    fix: correcting error message in createCarRent shape

--- src/shapes/cars/createCarRent.shape.ts
+++ src/shapes/cars/createCarRent.shape.ts
@@ -5,14 +5,14 @@ const createCarRentShape = yup.object().shape({
     .string()
     .matches(
       /^(0?[1-9]|[12][0-9]|3[01])[/](0?[1-9]|1[012])[/]\d{4}$/,
-      'Formato da data a ser enviada: dd/mm/yyyy'
+      'Invalid date format. Date must be sent as: "dd/mm/yyyy"'
     )
     .required(),
   rentalReturnDate: yup
     .string()
     .matches(
       /^(0?[1-9]|[12][0-9]|3[01])[/](0?[1-9]|1[012])[/]\d{4}$/,
-      'Formato da data a ser enviada: dd/mm/yyyy'
+      'Invalid date format. Date must be sent as: "dd/mm/yyyy"'
     )
     .required(),
 });
