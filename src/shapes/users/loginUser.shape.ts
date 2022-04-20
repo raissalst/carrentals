a886590b8d054364ce6c8abcb29efe2bab1fea64
Raissa Toledo <raissalst@gmail.com>
2022-04-20 11:39:28 -0300

    fix: changing some error messages from schemas

--- src/shapes/users/loginUser.shape.ts
+++ src/shapes/users/loginUser.shape.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 
 const loginUserShape = yup.object().shape({
   email: yup.string().email().lowercase().required(),
-  password: yup.string().min(4).required(),
+  password: yup.string().required(),
 });
 
 export default loginUserShape;
