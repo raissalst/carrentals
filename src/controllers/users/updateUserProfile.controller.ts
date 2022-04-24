48c61a40ce03a88562142603ba5b4f2f8aa73dae
Roberto Bressanelli <rbressanelli@gmail.com>
2022-04-23 21:08:35 -0300

    Feat: update profile code done

--- src/controllers/users/updateUserProfile.controller.ts
+++ src/controllers/users/updateUserProfile.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import { Address } from '../../entities/Address';
+import { User } from '../../entities/User';
 import { UserRepository } from '../../repositories';
 import {
   updateAddressProfileService,
@@ -8,7 +10,8 @@ import { handleError } from '../../utils';
 
 const updateUserProfileController = async (req: Request, res: Response) => {
   try {
-    let userAddressProfile;
+    let userAddressProfile: Array<Address>;
+    let userProfileData: Array<User>;
     const {
       name,
       email,
@@ -20,24 +23,32 @@ const updateUserProfileController = async (req: Request, res: Response) => {
       isActive,
       ...addressData
     } = req.validated;
-    // console.log(req.userAuth.user)
-    const { address, city, state, country, ...userData } = req.validated;
-    
+
+    let { address, city, state, country, ...userData } = req.validated;
+
+    if (Object.keys(userData).length === 0) {
+      userProfileData = await new UserRepository().findUserProfile(
+        req.userAuth.user.id
+      );
+      const { ...user } = userProfileData[0];
+      userData['name'] = user.name;
+    }
+
     const userProfile = await updateProfileService(
       userData,
       req.userAuth.user.id
     );
+
     if (Object.keys(addressData).length > 0) {
       userAddressProfile = await updateAddressProfileService(
         userProfile.addressId,
         addressData
-    );
-      userProfile['address'] = [userAddressProfile]
-
+      );
+      userProfile['address'] = [userAddressProfile];
     }
-    
+    const { addressId, ...updatedUserProfile } = userProfile;
 
-    return res.status(200).json(userProfile);
+    return res.status(200).json(updatedUserProfile);
   } catch (err: any) {
     return handleError(err, res);
   }
