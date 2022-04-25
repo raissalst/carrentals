9704f7872f0ffb3d43f4ca54ff149d586ffaf23d
Sidney Patez <patezsidney.jr@gmail.com>
2022-04-25 12:00:12 -0300

    feat: route create car finish

--- src/controllers/cars/createCar.controller.ts
+++ src/controllers/cars/createCar.controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
+import { CarRepository } from '../../repositories';
 
-const createCarController = (req: Request, res: Response) => {
-  res.status(201).json(req.validated);
+const createCarController = async (req: Request, res: Response) => {
+  const { userAuth, validated } = req;
+
+  const data = validated.cars.map((car) => {
+    return { ...car, companyId: userAuth.user.id };
+  });
+
+  const response = await new CarRepository().saveMultipleCars(data);
+
+  res.status(201).json(response);
 };
 
 export default createCarController;
