e22026f0a7f72f4492a5e79348a74e6b95e48997
Raissa Toledo <raissalst@gmail.com>
2022-04-25 16:12:40 -0300

    feat: changing car type in map of create car controller

--- src/controllers/cars/createCar.controller.ts
+++ src/controllers/cars/createCar.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
+import { Car } from '../../entities/Car';
 import { CarRepository } from '../../repositories';
 
 const createCarController = async (req: Request, res: Response) => {
   const { userAuth, validated } = req;
 
-  const data = validated.cars.map((car) => {
+  const data = validated.cars.map((car: Car) => {
     return { ...car, company: userAuth.user.id };
   });
 
