e297232c8df91c06ff9a58fb039eb508f31b9d31
Sidney Patez <patezsidney.jr@gmail.com>
2022-04-25 12:00:27 -0300

    feat: create car tests

--- src/tests/routes/cars/createCar.spec.ts
+++ src/tests/routes/cars/createCar.spec.ts
@@ -2,7 +2,7 @@ import { describe, it, expect, beforeAll, afterAll } from '@jest/globals';
 import { connection } from '../..';
 import app from '../../../app';
 import request from 'supertest';
-import { UserRepository } from '../../../repositories';
+import { CarRepository, UserRepository } from '../../../repositories';
 import { v4 } from 'uuid';
 import jwt from 'jsonwebtoken';
 import { jwtConfig } from '../../../configs';
@@ -20,10 +20,20 @@ describe('create car tests', () => {
     token: '',
     data: {},
   };
+
+  let adminToken;
   beforeAll(async () => {
     const newCompany = await createCompany();
     company.data = newCompany;
     company.token = jwt.sign({ user: newCompany }, jwtConfig.secretKey, {
+      expiresIn: jwtConfig.expiresIn,
+    });
+
+    const admin = await new UserRepository().findByEmail(
+      process.env.ADMIN_EMAIL
+    );
+
+    adminToken = jwt.sign({ user: admin }, jwtConfig.secretKey, {
       expiresIn: jwtConfig.expiresIn,
     });
   });
@@ -42,7 +52,7 @@ describe('create car tests', () => {
         gear: 'automatico',
         chassis: '3C8FY68B82T297664',
         currentMileage: 856,
-        rentalPricePerday: 95.0,
+        rentalPricePerDay: 95.0,
       },
     ],
   };
@@ -53,6 +63,42 @@ describe('create car tests', () => {
       .set('Authorization', `Bearer ${company.token}`);
 
     expect(response.statusCode).toBe(201);
+    expect(response.body).toHaveLength(1);
+
+    const car = await new CarRepository().getCarById(response.body[0].id);
+
+    expect(car).toBeTruthy();
+  });
+
+  it('401 UNAUTORIZED, try create car with no header', async () => {
+    const response = await request(app).post('/api/cars').send(reqMock);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toStrictEqual({
+      error: 'Missing authorization token',
+    });
+  });
+
+  it('401, when try create car with a admin token', async () => {
+    const response = await request(app)
+      .post('/api/cars')
+      .send(reqMock)
+      .set('Authorization', `Bearer ${adminToken}`);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toStrictEqual({ error: 'Unauthorized' });
+  });
+
+  it('400, when try create car with request not contains a array', async () => {
+    const otherMock = {
+      cars: reqMock.cars[0],
+    };
+    const response = await request(app)
+      .post('/api/cars')
+      .send(otherMock)
+      .set('Authorization', `Bearer ${company.token}`);
+
+    expect(response.statusCode).toBe(400);
   });
 });
 
