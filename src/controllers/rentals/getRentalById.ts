876cfc38a2ee84ed1d06d8cbbf505f79d5bcd6e6
Raissa Toledo <raissalst@gmail.com>
2022-04-26 12:41:45 -0300

    feat: test for get rental by id implemented

--- src/controllers/rentals/getRentalById.ts
+++ src/controllers/rentals/getRentalById.ts
@@ -6,22 +6,26 @@ const getRentalByIdController = async (
   req: Request,
   res: Response
 ): Promise<any> => {
-  const { id }: any = req.params;
+  try {
+    const { id }: any = req.params;
 
-  const rentalSearched = await new RentalRepository().getRentalById(id);
+    const rentalSearched = await new RentalRepository().getRentalById(id);
 
-  for (const item in rentalSearched) {
-    if (item.endsWith('Date') && rentalSearched[`${item}`]) {
-      rentalSearched[`${item}`] =
-        rentalSearched[`${item}`].toLocaleDateString('pt-BR');
+    for (const item in rentalSearched) {
+      if (item.endsWith('Date') && rentalSearched[`${item}`]) {
+        rentalSearched[`${item}`] =
+          rentalSearched[`${item}`].toLocaleDateString('pt-BR');
+      }
     }
-  }
 
-  if (!rentalSearched) {
-    throw new ErrorHandler(404, 'Rental not found.');
-  }
+    if (!rentalSearched) {
+      throw new ErrorHandler(404, 'Rental not found.');
+    }
 
-  return res.status(200).json(rentalSearched);
+    return res.status(200).json(rentalSearched);
+  } catch (err) {
+    return handleError(err, res);
+  }
 };
 
 export default getRentalByIdController;
