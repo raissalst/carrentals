625df0056ceee19b7d3bfd0a03e0a1dfb01409b1
Sidney Patez <patezsidney.jr@gmail.com>
2022-04-26 11:11:52 -0300

    fix: find error

--- src/middlewares/validateCustomer.middleware.ts
+++ src/middlewares/validateCustomer.middleware.ts
@@ -1,23 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
-import { User } from '../entities/User';
 import { UserRepository } from '../repositories';
 import { ErrorHandler, handleError } from '../utils';
 
-const validateCustomer = async (req: Request, res: Response, next: NextFunction) => {
+const validateCustomer = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const user = await new UserRepository().findByEmail(
-      (req.userAuth as User).email
-    )
+      req.userAuth.user.email
+    );
 
     if (user.userType === 'cliente') {
       return next();
     } else {
-      throw new ErrorHandler(401, 'Unauthorized')
+      throw new ErrorHandler(401, 'Unauthorized');
     }
-
   } catch (err: any) {
     return handleError(err, res);
-  }  
+  }
 };
 
 export default validateCustomer;
