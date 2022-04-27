c9e4a1c379883747a2ff7a390b163c3f879bc42c
Roberto Bressanelli <rbressanelli@gmail.com>
2022-04-27 09:15:31 -0300

    Feat: tests concluded

--- src/tests/routes/users/getUserProfileRentals.spec.ts
+++ src/tests/routes/users/getUserProfileRentals.spec.ts
@@ -2,18 +2,45 @@ import { expect, describe, it, beforeAll, afterAll } from '@jest/globals';
 import { connection } from '../..';
 import app from '../../../app';
 import request from 'supertest';
+import {
+  CarRepository,
+  RentalRepository,
+  UserRepository,
+} from '../../../repositories';
+import { v4 } from 'uuid';
+import jwt from 'jsonwebtoken';
+import { jwtConfig } from '../../../configs';
+import { User } from '../../../entities/User';
+import { Rental } from '../../../entities/Rental';
 
-beforeAll(async () => {
-  await connection.create();
-});
-
+let companyToken: string;
+let customerToken: string;
+let rentalsOpen: Rental;
+let rentalsClose: Rental
+let userCustomer: User;
+let rentals2: Rental;
+let userCompany: User;
+let user: User;
 afterAll(async () => {
   await connection.dropTables();
   await connection.close();
 });
 
 describe('get users rentals', () => {
+  const userMock = {
+    id: v4(),
+    name: 'Jhon Doe',
+    email: 'jhon@mail.com',
+    password: '1234',
+    cpf: '123.123.123-12',
+    phone: '1191234-1234',
+    userType: 'cliente',
+    isActive: true,
+    addressId: v4(),
+  };
+
   const mockCar = {
+    id: v4(),
     name: 'voyage',
     model: 'GTS',
     brand: 'VW',
@@ -29,20 +56,107 @@ describe('get users rentals', () => {
   };
 
   const companyMock = {
+    id: v4(),
     name: 'Company SA',
     email: 'companysa@gmail.com',
     password: '123456',
     phone: '21997771234',
     userType: 'empresa',
     cnpj: '29.594.702/0001-22',
-    address: 'Rua company, 100',
-    city: 'Petrópolis',
-    state: 'RJ',
-    country: 'Brasil',
+    addressId: v4(),
   };
 
-  const mockRental = {    
-    "rentalStartDate": "10/05/2022",
-    "rentalReturnDate": "15/05/2022",
-  }
-})
+  const mockRental = {
+    id: v4(),
+    rentalStartDate: '2021-10-05T03:00:00.000Z',
+    rentalReturnDate: '2021-10-07T03:00:00.000Z',
+    returnedCar: false,
+    rentalPricePerDay: 95.0,
+    rentalPricePreview: 475.0,
+    rentalPriceTotal: 0.0,
+    mileageRan: 0,
+    customer: userMock.id,
+    car: mockCar.id,
+  };
+
+  const mockRentalCompany = {
+    id: v4(),
+    rentalStartDate: '2021-10-05T03:00:00.000Z',
+    rentalReturnDate: '2021-10-07T03:00:00.000Z',
+    returnedCar: false,
+    rentalPricePerDay: 95.0,
+    rentalPricePreview: 475.0,
+    rentalPriceTotal: 0.0,
+    mileageRan: 0,
+    customer: companyMock.id,
+    car: mockCar.id,
+  };
+
+  beforeAll(async () => {
+    await connection.create();
+
+    userCustomer = await new UserRepository().saveUser(userMock as any);
+    userCompany = await new UserRepository().saveUser(companyMock as any);
+    const car = await new CarRepository().saveCar(mockCar as any);
+    rentalsOpen = await new RentalRepository().saveRental(mockRental as any);
+
+    mockRental.returnedCar = true
+    mockRental.id = v4()
+    rentalsClose = await new RentalRepository().saveRental(mockRental as any);
+    rentals2 = await new RentalRepository().saveRental(
+      mockRentalCompany as any
+    );
+  });
+
+  it('should retrieve customer rentals', async () => {
+    user = userCustomer;
+    customerToken = jwt.sign({ user }, jwtConfig.secretKey, {
+      expiresIn: jwtConfig.expiresIn,
+    });
+
+    const response = await request(app)
+      .get('/api/users/profile/rentals')
+      .set('Authorization', `Bearer ${customerToken}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0].id).toEqual(rentalsOpen.id);
+  });
+
+  it('should retrieve company rentals', async () => {
+    user = userCompany;
+    companyToken = jwt.sign({ user }, jwtConfig.secretKey, {
+      expiresIn: jwtConfig.expiresIn,
+    });
+
+    const response = await request(app)
+      .get('/api/users/profile/rentals')
+      .set('Authorization', `Bearer ${companyToken}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0].id).toEqual(rentals2.id);
+  });
+
+  it('should not retrieve rentals without bearer token', async () => {
+    const response = await request(app)
+      .get('/api/users/profile/rentals')
+      .set('Authorization', `Bearer`);
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toMatchObject({
+      error: 'Missing authorization token',
+    });
+  });
+
+  it('should list opened rentals by returnedCar query equals to false', async () => {
+    user = userCustomer;
+    customerToken = jwt.sign({ user }, jwtConfig.secretKey, {
+      expiresIn: jwtConfig.expiresIn,
+    });
+
+    const response = await request(app)
+      .get('/api/users/profile/rentals')
+      .set('Authorization', `Bearer ${customerToken}`)
+      .query('returnedCar=false');
+    
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0].returnedCar).toStrictEqual(false);
+  });
+});
