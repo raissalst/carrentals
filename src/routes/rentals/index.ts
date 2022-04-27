ad1144f5b033bf154eed74c8cc53d26b28b26701
Sidney Patez <patezsidney.jr@gmail.com>
2022-04-27 14:04:03 -0300

    feat: add return car to routes

--- src/routes/rentals/index.ts
+++ src/routes/rentals/index.ts
@@ -1,12 +1,29 @@
 import { Router } from 'express';
-import { validateAuth, validateAdmin } from '../../middlewares';
-import { getRentalByIdController } from '../../controllers';
+import {
+  validateAuth,
+  validateAdmin,
+  validateCompany,
+  validateShape,
+} from '../../middlewares';
+import {
+  getRentalByIdController,
+  returnCarController,
+} from '../../controllers';
 import { getAllRentalsController } from '../../controllers';
+import { returnCarShape } from '../../shapes';
 
 const rentalRoute = Router();
 
 rentalRoute.get('', validateAuth, validateAdmin, getAllRentalsController);
 
 rentalRoute.get('/:id', validateAuth, validateAdmin, getRentalByIdController);
+
+rentalRoute.post(
+  '/:id',
+  validateShape(returnCarShape),
+  validateAuth,
+  validateCompany,
+  returnCarController
+);
 
 export default rentalRoute;
