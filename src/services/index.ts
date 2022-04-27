a9a0e82926cdb82d635626ba71716056912b294a
Sidney Patez <patezsidney.jr@gmail.com>
2022-04-27 15:00:24 -0300

    fix: duplicated foldes

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,7 +5,7 @@ import getProfileCarsService from './users/getProfileCars.service';
 
 import updateProfileService from './users/updateProfile.service';
 import updateAddressProfileService from './address/updateProfile.service';
-import createRentalService from './rental/createRental.service';
+import createRentalService from './rentals/createRental.service';
 import getAllUsersService from './users/getAllUsers.service';
 
 import getAllRentalsService from './rentals/getAllRentals.service';
