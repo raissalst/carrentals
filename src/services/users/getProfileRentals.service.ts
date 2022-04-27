e756828fc9faf5c39598d510d387342889693ad1
Roberto Bressanelli <rbressanelli@gmail.com>
2022-04-27 15:03:43 -0300

    Fix: changed code considering observations

--- src/services/users/getProfileRentals.service.ts
+++ src/services/users/getProfileRentals.service.ts
@@ -1,23 +1,32 @@
 import { User } from '../../entities/User';
 import { RentalRepository } from '../../repositories';
-import { changeDateFormat, ErrorHandler } from '../../utils';
+import { ErrorHandler } from '../../utils';
 import { carObjectKeyFilter } from '../../utils';
 
 const getProfileRentalsService = async (user: User, closedRental: any) => {
-  
   const closedRentalQuery = closedRental?.trim().toLowerCase();
-  
-  if (closedRentalQuery && closedRentalQuery !== 'true' && closedRentalQuery !== 'false') {
+
+  if (
+    closedRentalQuery &&
+    closedRentalQuery !== 'true' &&
+    closedRentalQuery !== 'false'
+  ) {
     throw new ErrorHandler(400, 'Query params informed must be true or false.');
   }
   const rentals = await new RentalRepository().findRentals(
     user.id,
     closedRental as boolean
   );
-  
+
   carObjectKeyFilter(user.userType, rentals);
 
-  changeDateFormat(rentals)
+  for (const item of rentals) {
+    for (const rentalItem in item) {
+      if (rentalItem.endsWith('Date') && item[rentalItem]) {        
+        item[rentalItem] = item[rentalItem].toLocaleDateString('pt-BR');
+      }
+    }
+  }
 
   return rentals;
 };
