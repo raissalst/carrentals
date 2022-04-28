0265ef8564447349dc947d99cb2518cc8e0f6375
Sidney Patez <patezsidney.jr@gmail.com>
2022-04-28 10:29:50 -0300

    feat: return car tests

--- src/tests/shapes/rentals/returnCar.spec.ts
+++ src/tests/shapes/rentals/returnCar.spec.ts
@@ -3,17 +3,17 @@ import { returnCarShape } from '../../../shapes';
 
 describe('return car shape tests', () => {
   it('try a valid schema', async () => {
-    const isValid = await returnCarShape.isValid({ mileageRun: 100 });
+    const isValid = await returnCarShape.isValid({ mileageRan: 100 });
 
     expect(isValid).toBe(true);
   });
   it('try schema with incorrect key', async () => {
-    const isValid = await returnCarShape.isValid({ mileangeRun: 100 });
+    const isValid = await returnCarShape.isValid({ mileangeRan: 100 });
 
     expect(isValid).toBe(false);
   });
   it('try schema with incorrect value', async () => {
-    const isValid = await returnCarShape.isValid({ mileageRun: -100 });
+    const isValid = await returnCarShape.isValid({ mileageRan: -100 });
 
     expect(isValid).toBe(false);
   });
